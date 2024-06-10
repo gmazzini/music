60e800667b55cfa5b96feb69845aab0003c78c47
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-10 07:23:06 +0200

    Update show.php

--- show.php
+++ show.php
@@ -2,12 +2,14 @@
 include "local.php";
 $liv=$_GET["liv"];
 $idin=$_GET["id"];
+$idorg$_GET["idorg"];
 if(!isset($liv)){$liv=1; $idin="root";}
+if(!isset($idorg))$idorg="root";
 if($liv<3){$nextliv=$liv+1; $db="music";}
 else $db="song";
 if($liv>1)$prevliv=$liv-1;
 else $prevliv=$liv;
-echo "<pre>LIV: $liv, ID: $idin <a href='show.php?liv=$prevliv&id=$idin'>Prev</a>\n";
+echo "<pre>LIV: $liv, ID: $idin <a href='show.php?liv=$prevliv&id=$idorg'>Prev</a>\n";
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $query=mysqli_query($con,"select id,name from $db where top='$idin' order by name");
 for(;;){
@@ -15,7 +17,7 @@ for(;;){
   if($row==null)break;
   $id=$row["id"];
   $name=$row["name"];
-  echo "<a href='show.php?liv=$nextliv&id=$id'>$name</a>\n";
+  echo "<a href='show.php?liv=$nextliv&id=$id&idorg=$idin'>$name</a>\n";
 }
 echo "</pre>";
 mysqli_free_result($query);
