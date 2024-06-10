472fd2ee09f48eafb223b098787df8bd059d6644
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-10 07:13:45 +0200

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@ $idin=$_GET["id"];
 if(!isset($liv)){$liv=1; $idin="root";}
 echo "<pre>LIV: $liv, ID: $idin\n";
 if($liv<3){$nextliv=$liv+1; $db="music";}
-else db="song";
+else $db="song";
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $query=mysqli_query($con,"select id,name from $db where top='$idin' order by name");
 for(;;){
