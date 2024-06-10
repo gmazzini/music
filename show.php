b9c1560a90057e12d7fea8ac51132802d2d07a7f
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-10 06:41:57 +0200

    Create show.php

---
+++ show.php
@@ -0,0 +1,17 @@
+<?php
+include "local.php";
+echo "<pre>";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+$query=mysqli_query($con,"select id,name from music where top='root'");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $id=$row["id"];
+  $name=$row["name"];
+  echo "$id $name\n";
+  }
+}
+echo "</pre>";
+mysqli_free_result($query);
+mysqli_close($con);
+?>
