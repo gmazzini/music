108adb038684cf4f9ddeb36204a61b63bd2d2abe
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-10 07:03:16 +0200

    Update show.php

--- show.php
+++ show.php
@@ -1,14 +1,14 @@
 <?php
 include "local.php";
-echo "<pre>Link LIV1\n";
+echo "<pre>";
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
-$query=mysqli_query($con,"select id,name from music where top='root'");
+$query=mysqli_query($con,"select id,name from music where top='root' order by name");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $id=$row["id"];
   $name=$row["name"];
-  echo "<a>$name</a>\n";
+  echo "<a href='show.php?liv=2&id=$id'>$name</a>\n";
 }
 echo "</pre>";
 mysqli_free_result($query);
