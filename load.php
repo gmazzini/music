e217596116a839e633af407a6e79256e18e22acf
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-11 17:29:16 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,6 +1,9 @@
 <?php
+include "local.php";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $access_token=file_get_contents("access_token");
 @$id=$_GET["id"];
+mysqli_query($con,"update song set played=played+1 where id='$id'");
 if(!file_exists("cached/$id")){
   $ch=curl_init();
   curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?alt=media");
@@ -15,4 +18,5 @@ $aux=file_get_contents("cached/$id");
 header('Content-type: audio/mpeg;');
 header("Content-Length: ".strlen($aux));
 echo $aux;
+mysqli_close($con);
 ?>
