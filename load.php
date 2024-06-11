8e0466405cb71b4cb03bbd74640663f3805b0897
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-11 16:32:42 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,4 +1,16 @@
 <?php
 shell_exec("scp admgm02@100.100.0.30:/home/www/data/access_token /home/www/music.mazzini.org");
-
+$access_token=file_get_contents("access_token");
+@$idin=$_GET["id"];
+if(!file_exists("cached/$id")){
+  $ch=curl_init();
+  curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?alt=media");
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Authorization: Bearer ".$access_token));
+  $oo=curl_exec($ch);
+  curl_close($ch);
+  file_put_contents("cached/$id",$oo);
+}
+  
 ?>
