2b02026b244b19b8760902c61b2a50cbda7e711a
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-12 05:51:32 +0200

    Update load.php

--- load.php
+++ load.php
@@ -3,8 +3,9 @@ include "local.php";
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $access_token=file_get_contents("access_token");
 @$id=$_GET["id"];
+$ffname="cached/$id";
 mysqli_query($con,"update song set played=played+1 where id='$id'");
-if(!file_exists("cached/$id")){
+if(!file_exists($ffname)){
   $ch=curl_init();
   curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?alt=media");
   curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
@@ -12,9 +13,13 @@ if(!file_exists("cached/$id")){
   curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Authorization: Bearer ".$access_token));
   $oo=curl_exec($ch);
   curl_close($ch);
-  file_put_contents("cached/$id",$oo);
+  file_put_contents($ffname,$oo);
 }
-$aux=file_get_contents("cached/$id");
+if(filesize($ffname)<1000000){
+  unlink($ffname);
+  $ffname="Heartbeat.mp3";
+}
+$aux=file_get_contents($ffname);
 header('Content-type: audio/mpeg;');
 header("Content-Length: ".strlen($aux));
 echo $aux;
