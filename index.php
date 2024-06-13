611b0d305f4b74522e3c0b88ca9c5883876b13e8
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-13 06:21:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,16 +29,16 @@ for($ipl=0;;$ipl++){
 }
 mysqli_free_result($query);
 
-echo "<pre><a href='?liv=1&pwdmd5=$pwdmd5&go=NAV'>NAV</a>";
+echo "<pre><a href='?liv=1&pwdmd5=$pwdmd5&go=DIR'>DIRECTORY</a>";
+echo " <a href='?pwdmd5=$pwdmd5&go=SRC'>SEARCH</a>";
 echo " <a href='?pwdmd5=$pwdmd5&go=LST'>LIST</a>";
-echo " <a href='?pwdmd5=$pwdmd5&go=PLY'>PLAY</a>";
-echo "</pre><hr>";
+echo " <a href='?pwdmd5=$pwdmd5&go=PLY'>PLAY</a></pre><hr>";
 
-if($go=="")$go="NAV";
+if($go=="")$go="PLY";
 switch($go){
   
-  // navigation
-  case "NAV":
+  // directory
+  case "DIR":
   if($liv=="")$liv=1;
   switch($liv){
     case 1:
@@ -89,14 +89,14 @@ switch($go){
     $db="song";
     break;
   }
-  echo "<pre>$first liv:$liv, idin:$idin idprev:$idprev <a href='?liv=$prevliv&idin=$idprev&pwdmd5=$pwdmd5&go=NAV'>Prev</a>\n";
+  echo "<pre>$first liv:$liv, idin:$idin idprev:$idprev <a href='?liv=$prevliv&idin=$idprev&pwdmd5=$pwdmd5&go=DIR'>Prev</a>\n";
   $query=mysqli_query($con,"select id,name from $db where parent='$idin' order by name");
   for(;;){
     $row=mysqli_fetch_assoc($query);
     if($row==null)break;
     $id=$row["id"];
     $name=$row["name"];
-    if($liv<3)echo "<a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&go=NAV'>$name</a>\n";
+    if($liv<3)echo "<a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&go=DIR'>$name</a>\n";
     else {
       echo "$name";
       for($i=0;$i<$ipl;$i++){
@@ -105,8 +105,112 @@ switch($go){
         $row1=mysqli_fetch_assoc($query1);
         $position=(int)$row1["position"];
         mysqli_free_result($query1);
-        if($position==0)echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=1&go=NAV'>+$apl</a>";
-        else echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=2&go=NAV'>-$apl</a>";
+        if($position==0)echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=1&go=DIR'>+$apl</a>";
+        else echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=2&go=DIR'>-$apl</a>";
+      }
+      echo "\n";
+    }
+  }
+  echo "</pre>";
+  mysqli_free_result($query);
+  break;
+
+   // search
+  case "SRC":
+  echo "<form action=\"?pwdmd5=$pwdmd5&go=SRC\">";
+  echo "search <input type=text name=search size=16>";
+  echo "<input type=submit name=act value=Enter>";
+  echo "</form>";
+  break;
+
+   $query=mysqli_query($con,"select id,position from playlist where pwdmd5='$pwdmd5' and label='$plin' order by position");
+  for(;;){
+    $row=mysqli_fetch_assoc($query);
+    if($row==null)break;
+    $id=$row["id"];
+    $position=$row["position"];
+    $query1=mysqli_query($con,"select name,parent from song where id='$id'");
+    $row1=mysqli_fetch_assoc($query1);
+    $name=$row1["name"];
+    $parent=$row1["parent"];
+    mysqli_free_result($query1);
+    $query1=mysqli_query($con,"select name,parent from music where id='$parent'");
+    $row1=mysqli_fetch_assoc($query1);
+    $liv2=$row1["name"];
+    $parent=$row1["parent"];
+    mysqli_free_result($query1);
+    $query1=mysqli_query($con,"select name,parent from music where id='$parent'");
+    $row1=mysqli_fetch_assoc($query1);
+    $liv1=$row1["name"];
+    $parent=$row1["parent"];
+    mysqli_free_result($query1);
+    echo "<a href=?act=C&pl=$plin&pwdmd5=$pwdmd5&pos=$position&go=LST>C</a> ";
+    echo "<a href=?act=U&pl=$plin&pwdmd5=$pwdmd5&pos=$position&go=LST>U</a> ";
+    echo "<a href=?act=D&pl=$plin&pwdmd5=$pwdmd5&pos=$position&go=LST>D</a> ";
+    echo " $position | $id | $name | $liv2 | $liv1\n";
+  }
+
+
+  
+ 
+    $query=mysqli_query($con,"select parent from music where id='$idin'");
+    $row=mysqli_fetch_assoc($query);
+    $idprev=$row["parent"];
+    mysqli_free_result($query);
+    $prevliv=1;
+    $nextliv=3;
+    $db="music";
+    break;
+    case 3:
+    $query=mysqli_query($con,"select parent from music where id='$idin'");
+    $row=mysqli_fetch_assoc($query);
+    $idprev=$row["parent"];
+    mysqli_free_result($query);
+    $prevliv=2;
+    $nextliv=4;
+    $db="song";
+    break;
+    case 4:
+    if($pla==1){
+      $query=mysqli_query($con,"select max(position) from playlist where label='$plin' and pwdmd5='$pwdmd5'");
+      $row=mysqli_fetch_row($query);
+      $pllast=1+(int)$row[0];
+      mysqli_free_result($query);
+      mysqli_query($con,"insert into playlist (pwdmd5,id,position,label) values ('$pwdmd5','$idin',$pllast,'$plin')");
+    }
+    elseif($pla==2)mysqli_query($con,"delete from playlist where label='$plin' and pwdmd5='$pwdmd5' and id='$idin'");
+    $query=mysqli_query($con,"select parent from song where id='$idin'");
+    $row=mysqli_fetch_assoc($query);
+    $idin=$row["parent"];
+    mysqli_free_result($query);
+    $query=mysqli_query($con,"select parent from music where id='$idin'");
+    $row=mysqli_fetch_assoc($query);
+    $idprev=$row["parent"];
+    mysqli_free_result($query);
+    $prevliv=2;
+    $nextliv=4;
+    $liv=3;
+    $db="song";
+    break;
+  }
+  echo "<pre>$first liv:$liv, idin:$idin idprev:$idprev <a href='?liv=$prevliv&idin=$idprev&pwdmd5=$pwdmd5&go=DIR'>Prev</a>\n";
+  $query=mysqli_query($con,"select id,name from $db where parent='$idin' order by name");
+  for(;;){
+    $row=mysqli_fetch_assoc($query);
+    if($row==null)break;
+    $id=$row["id"];
+    $name=$row["name"];
+    if($liv<3)echo "<a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&go=DIR'>$name</a>\n";
+    else {
+      echo "$name";
+      for($i=0;$i<$ipl;$i++){
+        $apl=$pl[$i];
+        $query1=mysqli_query($con,"select position from playlist where label='$apl' and pwdmd5='$pwdmd5' and id='$id'");
+        $row1=mysqli_fetch_assoc($query1);
+        $position=(int)$row1["position"];
+        mysqli_free_result($query1);
+        if($position==0)echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=1&go=DIR'>+$apl</a>";
+        else echo " <a href='?liv=$nextliv&idin=$id&pwdmd5=$pwdmd5&pl=$apl&pla=2&go=DIR'>-$apl</a>";
       }
       echo "\n";
     }
