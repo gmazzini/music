e619fdb4ef3e3176c62517b86fde5709f60239d2
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-15 06:31:44 +0200

    Update songdetails.php

--- songdetails.php
+++ songdetails.php
@@ -6,16 +6,16 @@ for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $id=$row["id"];
-  $name=$row["name"];
+  $name=mysqli_real_escape_string($con,$row["name"]);
   $parent=$row["parent"];
   $query1=mysqli_query($con,"select name,parent from music where id='$parent'");
   $row1=mysqli_fetch_assoc($query1);
-  $liv2=$row1["name"];
+  $liv2=mysqli_real_escape_string($con,$row1["name"]);
   $parent=$row1["parent"];
   mysqli_free_result($query1);
   $query1=mysqli_query($con,"select name from music where id='$parent'");
   $row1=mysqli_fetch_assoc($query1);
-  $liv1=$row1["name"];
+  $liv1=mysqli_real_escape_string($con,$row1["name"]);
   mysqli_free_result($query1);
   mysqli_query($con,"update song set title='$name',album='$liv2',artist='$liv1' where id='$id'");
 }
