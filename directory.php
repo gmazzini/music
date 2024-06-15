c926b9febb609ba00f205fb7e29e9673bf203ecb
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-15 23:02:03 +0200

    Update directory.php

--- directory.php
+++ directory.php
@@ -13,6 +13,7 @@ curl_close($ch);
 $files=$oo["files"];
 foreach($files as $k => $v){
   $id=$v["id"];
+  echo "$id\n";
   $ch2=curl_init();
   curl_setopt($ch2,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files?q=$id&pageSize=500");
   curl_setopt($ch2,CURLOPT_RETURNTRANSFER,1);
@@ -23,7 +24,7 @@ foreach($files as $k => $v){
   $files2=$oo2["files"];
   foreach($files2 as $k2 => $v2){
     $id=$v2["id"];
-    echo "$id $id2\n";
+    echo "\t$id2\n";
   }
 }
 mysqli_close($con);
