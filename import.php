af95a054485a31861a8e7d63f250f68a9374dffc
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-17 13:39:10 +0200

    Update import.php

--- import.php
+++ import.php
@@ -7,8 +7,43 @@ for(;;){
   if($row==null)break;
   $id=$row["id"];
   echo "$id\n";
-  file_get_contents("https://music.mazzini.org/load.php?id=$id");
-  sleep(5);
+  $access_token=file_get_contents("access_token");
+  $ffname="cached/$id";
+  if(!file_exists($ffname)){
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?alt=media");
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Authorization: Bearer ".$access_token));
+    $oo=curl_exec($ch);
+    curl_close($ch);
+    file_put_contents($ffname,$oo);
+    $oo=json_decode(shell_exec("ffprobe -v quiet -print_format json -show_format $ffname"),true);
+    $ooo=$oo["format"];
+    $duration=(int)$ooo["duration"];
+    $format=$ooo["format_name"];
+    $aux=$ooo["tags"]["title"];
+    $aux=preg_replace("/[ ]{0,}\([^)]+\)[ ]{0,}/","",$aux);
+    $aux=preg_replace("/[ ]{0,}\[[^)]+\][ ]{0,}/","",$aux);
+    $title=mysqli_real_escape_string($con,$aux);
+    $aux=$ooo["tags"]["album"];
+    $aux=preg_replace("/[ ]{0,}\([^)]+\)[ ]{0,}/","",$aux);
+    $aux=preg_replace("/[ ]{0,}\[[^)]+\][ ]{0,}/","",$aux);
+    $album=mysqli_real_escape_string($con,$aux);
+    $aux=$ooo["tags"]["artist"];
+    $aux=preg_replace("/[ ]{0,}\([^)]+\)[ ]{0,}/","",$aux);
+    $aux=preg_replace("/[ ]{0,}\[[^)]+\][ ]{0,}/","",$aux);
+    $artist=mysqli_real_escape_string($con,$aux);
+    if($format!="mp3"){
+      unlink($ffname);
+      mysqli_query($con,"update song set nomp3=1 where id='$id'");
+    }
+    else {
+      $md5=md5_file($ffname,false);
+      mysqli_query($con,"update song set played=played+1,duration=$duration,title='$title',album='$album',artist='$artist',md5='$md5' where id='$id'");
+    }
+    sleep(5);
+  }
 }
 mysqli_free_result($query);
 mysqli_close($con);
