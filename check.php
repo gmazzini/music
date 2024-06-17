320abfb743d805f254db753895cae08afc84e5de
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-17 22:32:02 +0200

    Update check.php

--- check.php
+++ check.php
@@ -25,6 +25,18 @@ for(;;){
     elseif($i==1 && $album==$name)echo "Ok\n";
     elseif($i==2 && $artist==$name)echo "Ok\n";
     elseif($i==3 && "Music"==$name)echo "Ok\n";
+    if($i==1 && $album!=$name){
+      $ch=curl_init();
+      curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id");
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      curl_setopt($ch,CURLOPT_POST,1);
+      curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+      curl_setopt($ch,CURLOPT_HTTPHEADER,array("Content-Type: application/json","Authorization: Bearer ".$access_token));
+      curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PATCH");
+      curl_setopt($ch,CURLOPT_POSTFIELDS,'{"name": "'.$album.'"}');
+      $oo=json_decode(curl_exec($ch),true);
+      curl_close($ch);
+    }
     $ch=curl_init();
     curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?fields=parents");
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
