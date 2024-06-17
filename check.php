12a5d89421acbafed97a09249a7ba84897bb0373
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-17 22:36:03 +0200

    Update check.php

--- check.php
+++ check.php
@@ -22,9 +22,19 @@ for(;;){
     $name=$oo["name"];
     echo "$i | $id | $name\n";
     if($i==0 && $title==$name)echo "Ok\n";
-    elseif($i==1 && $album==$name)echo "Ok\n";
-    elseif($i==2 && $artist==$name)echo "Ok\n";
-    elseif($i==3 && "Music"==$name)echo "Ok\n";
+    if($i==0 && $title!=$name) {
+      $ch=curl_init();
+      curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id");
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+      curl_setopt($ch,CURLOPT_POST,1);
+      curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+      curl_setopt($ch,CURLOPT_HTTPHEADER,array("Content-Type: application/json","Authorization: Bearer ".$access_token));
+      curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PATCH");
+      curl_setopt($ch,CURLOPT_POSTFIELDS,'{"name": "'.$title.'"}');
+      $oo=json_decode(curl_exec($ch),true);
+      curl_close($ch);
+    }
+    if($i==1 && $album==$name)echo "Ok\n";
     if($i==1 && $album!=$name){
       $ch=curl_init();
       curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id");
@@ -37,6 +47,8 @@ for(;;){
       $oo=json_decode(curl_exec($ch),true);
       curl_close($ch);
     }
+    if($i==2 && $artist==$name)echo "Ok\n";
+    if($i==3 && "Music"==$name)echo "Ok\n";
     $ch=curl_init();
     curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$id?fields=parents");
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
