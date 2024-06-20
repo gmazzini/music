87255189edff0199fbbf97e76760e8f5d97a71ab
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-20 11:31:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "body {background-color: #F9F4B7; font-size: 30px;}\n";
 echo ".mybut {background-color: #666699; font-size: 30px; border: none; color: #F9F4B7; padding: 3px 3px; text-align: center; text-decoration: none;\n";
 echo "display: inline-block; font-size: 30px; border-radius: 3px; outline: none; }\n";
 echo ".mybut:hover {background-color: #FF0000; }\n";
-echo "form {display: inline-block; padding: 0px 0px; margin: 0px 0px;}\n";
+echo "form {display: inline-block; padding: 0px 0px; margin: 0px 0px; font-size: 30px;}\n";
 echo "input[type=submit] {padding: 2px 2px; margin: 2px 0px; cursor: pointer; background-color: #FF0000; font-size: 30px; color: white; border: none; border-radius: 4px;}\n";
 echo "</style>\n";
 
