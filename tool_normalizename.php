93db77cddc2cf35d0270cd998c98d9f0ec22dd53
Gianluca Mazzini <gianluca@mazzini.org>
2024-06-28 06:40:03 +0200

    Create tool_normalizename.php

---
+++ tool_normalizename.php
@@ -0,0 +1,17 @@
+<?php
+include "local.php";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+$query=mysqli_query($con,"select id,title,album,artist from login where pwdmd5='$pwdmd5'");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $id=$row["id"];
+  $title=$row["title];
+  $album=$row["album"];
+  $artist=$row["artist"];
+  $pp=strpos($album,$artist.": ",);
+  if($pp!==false)echo "$album\n";
+}
+mysqli_free_result($query);
+mysqli_close($con);
+?>
